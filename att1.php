ef5416860aa7eff82962bfd5731e4719646b0650
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-07-23 15:31:57 -0300

    Update att1.php

--- att1.php
+++ att1.php
@@ -23,3 +23,16 @@ class Monitor
         print "Monitor mostrando imagem";
     }
 }
+
+//programa principal
+
+//criei um objeto
+$monitor0 = new Monitor(); 
+//defini os atributos do objeto
+$monitor0 -> cor = "preto";
+$monitor0 -> resolucao = "1080x1920";
+$monitor0 -> marca = "AOC";
+//chamei os metodos
+$monitor0->ligar();
+$monitor0->mostrarImagem();
+$monitor0->desligar();
